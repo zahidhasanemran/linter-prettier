be8022079a679b25d6e0f88611f769a5a8620bdf
Md Zahid Hasan Emran <36685490+zahidhasanemran@users.noreply.github.com>
2023-01-29 01:54:55 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+
+React and Next Js
+
+
+{
+  "arrowParens": "avoid",
+  "bracketSpacing": true,
+  "htmlWhitespaceSensitivity": "strict",
+  "printWidth": 80,
+  "semi": false,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "es5",
+  "useTabs": false
+}
+
+
+Node.js
+
+{
+  "arrowParens": "avoid",
+  "bracketSpacing": true,
+  "printWidth": 80,
+  "semi": false,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "es5",
+  "useTabs": false
+}
